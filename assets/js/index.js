961e78a0781a8134f7fefed904517b8c94be129f
JagMolar <juana.garmue@educa.jcyl.es>
2022-09-23 20:34:15 +0200

    fixed styles and texts. English language files included

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,7 +26,36 @@ function sendEmail() {
 				document.getElementById("contacto").reset();
 			
 			}
-	} 
+} 
+
+function sendingEmail() {
+	
+	var nombre = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var m = document.getElementById("message").value;
+	var robot = document.getElementById("demo-human").checked;
+	var texto = m;
+	// console.log(nombre);
+	// console.log(texto);
+	// console.log(robot);
+	if (nombre.length<1 || email.length<1 || m.length<1 || robot == false) {
+			alert("Review the form.\nCheck the fields and check.");  
+		} else {
+			alert("There is a problem from the server.\To avoid errors, we will redirect you to your mail client.");  
+
+			let link = document.createElement('a')
+
+			link.href="mailto:juangmuelas@gmail.com?subject=Email from my Personal website&body=Hi, I'm  "+nombre+
+			" and my email: "+email+
+			"\n. \nSubject: "+texto
+			link.click()
+
+			alert("Thank you "+nombre+" for using my contact area. \nYour message has been successfully forwarded.")
+			  
+			document.getElementById("contacto").reset();
+		
+		}
+} 
 
 function test(){
 console.log("funciona");
